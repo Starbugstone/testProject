62904effd5c701adcde284295fadc200238f6358
StarbugStone <pint2stone@gmail.com>
2018-12-01 11:10:51 +0100

    Adding data base objects

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,29 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Twig\Environment;
+
 
 class HomeController extends AbstractController
 {
-    /**
-     * @var Environment
-     */
-    private $twig;
-
-    public function __construct(Environment $twig)
-    {
-
-        $this->twig = $twig;
-    }
 
     /**
      * @Route("/", name="home")
+     * @return Response
      */
     public function index():Response
     {
-        /*return $this->render('home/index.html.twig', [
+        return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-        ]);*/
-        return new Response($this->twig->render('home/index.html.twig'));
+        ]);
+
     }
 }
--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -3,17 +3,24 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class PropertyController
+ * @package App\Controller
+ */
 class PropertyController extends AbstractController
 {
     /**
-     * @Route("/property", name="property")
+     * @Route("/biens", name="property.index")
+     * @return Response
      */
-    public function index()
+    public function index(): Response
     {
         return $this->render('property/index.html.twig', [
             'controller_name' => 'PropertyController',
+            'current_menu' => 'properties',
         ]);
     }
 }
