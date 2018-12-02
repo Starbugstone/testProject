66ed9bfad41878019970060aa572c24f3da5ccd8
StarbugStone <pint2stone@gmail.com>
2018-12-02 12:02:17 +0100

    CRUD property finished

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * Class PropertyController
+ * Class AdminPropertyController
  * @package App\Controller
  */
 class PropertyController extends AbstractController
